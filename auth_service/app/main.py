9092cf5d9406f5a6e01fc67cc150977783fc2644
ftaboadar <f.taboada@uniandes.com.co>
2025-10-18 14:30:57 -0500

    ajustes en la infraestructura adicion de observavilidad y routing

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
-from app.api.v1 import api_router
-from app.db.session import init_db, close_db  
 from contextlib import asynccontextmanager
-from app.core.auth_middleware import AuthMiddleware
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
+from app.api.v1 import api_router
+from app.db.session import init_db, close_db
+from app.core.auth_middleware import AuthMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Evento de arranque
-    await init_db()
-    yield
-    # Evento de apagado
-    await close_db()
-
+    await init_db()          
+    try:
+        yield
+    finally:
+        await close_db()  
 
 app = FastAPI(
     lifespan=lifespan,
@@ -22,16 +22,20 @@ app = FastAPI(
     docs_url="/auth/docs",
     redoc_url="/auth/redoc",
     openapi_url="/auth/openapi.json",
-    )
+)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],          
-    allow_credentials=True,         
-    allow_methods=["*"],            
-    allow_headers=["*"],            
-    expose_headers=["*"]
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
 )
-app.add_middleware(AuthMiddleware)
+app.add_middleware(AuthMiddleware) 
 
 app.include_router(api_router, prefix="/auth/api/v1")
+
+@app.on_event("startup")
+async def _startup():
+    Instrumentator().instrument(app).expose(app, endpoint="/auth/metrics", include_in_schema=False)
